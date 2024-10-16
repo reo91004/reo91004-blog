514148649d290c0a265bcee449127416bad1b1fc
reo91004 <reo91004@gmail.com>
2024-10-16 10:40:10 +0900

    feat: 이름 -> 게시물

--- pages/api/social-image.tsx
+++ pages/api/social-image.tsx
@@ -24,7 +24,7 @@ export default async function handler(req: NextRequest) {
     const { properties, cover } = await result.json();
 
     const image = cover?.external?.url || cover?.file?.url || siteConfig.defaultPageCover;
-    const title = properties?.['이름']?.title?.[0]?.plain_text || siteConfig.name;
+    const title = properties?.['게시물']?.title?.[0]?.plain_text || siteConfig.name;
     const description = properties?.['설명']?.rich_text?.[0]?.plain_text || siteConfig.description;
     const tags = (properties?.['태그']?.multi_select || []).map((tag: any) => tag.name);
     const author = siteConfig.author;
