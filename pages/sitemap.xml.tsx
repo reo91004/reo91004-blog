6694c998d6a8975dbee1c6eadb20bc8720b633cc
reo91004 <reo91004@gmail.com>
2024-10-17 18:50:25 +0900

    feat: sitemap.xml.tsx 원본으로 수정

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,61 +1,54 @@
-import { NextRequest } from 'next/server';
+import type { GetServerSideProps } from 'next';
+
 import { host } from 'lib/config';
 import { getSiteMap } from 'lib/get-site-map';
 import type { SiteMap } from 'lib/types';
 
-export const config = {
-  runtime: 'edge',
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  if (req.method !== 'GET') {
+    res.statusCode = 405;
+    res.setHeader('Content-Type', 'application/json');
+    res.write(JSON.stringify({ error: 'method not allowed' }));
+    res.end();
+    return {
+      props: {},
+    };
+  }
+
+  const siteMap = await getSiteMap();
+
+  // cache for up to 8 hours
+  res.setHeader('Cache-Control', 'public, max-age=28800, stale-while-revalidate=28800');
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(createSitemap(siteMap));
+  res.end();
+
+  return {
+    props: {},
+  };
 };
 
-export default async function handler(req: NextRequest) {
-  if (req.method !== 'GET') {
-    return new Response(JSON.stringify({ error: 'method not allowed' }), {
-      status: 405,
-      headers: { 'Content-Type': 'application/json' },
-    });
-  }
+const createSitemap = (siteMap: SiteMap) =>
+  `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>${host}</loc>
+    </url>
 
-  const encoder = new TextEncoder();
-  const stream = new TransformStream();
-  const writer = stream.writable.getWriter();
+    <url>
+      <loc>${host}/</loc>
+    </url>
 
-  const writeChunk = async (chunk: string) => {
-    await writer.write(encoder.encode(chunk));
-  };
+    ${Object.keys(siteMap.canonicalPageMap)
+      .map(canonicalPagePath =>
+        `
+          <url>
+            <loc>${host}/${canonicalPagePath}</loc>
+          </url>
+        `.trim(),
+      )
+      .join('')}
+  </urlset>
+`;
 
-  // XML 헤더 작성
-  await writeChunk(`<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>${host}</loc>
-  </url>
-  <url>
-    <loc>${host}/</loc>
-  </url>`);
-
-  try {
-    const siteMap: SiteMap = await getSiteMap();
-
-    for (const canonicalPagePath of Object.keys(siteMap.canonicalPageMap)) {
-      await writeChunk(`
-  <url>
-    <loc>${host}/${encodeURIComponent(canonicalPagePath)}</loc>
-  </url>`);
-    }
-  } catch (error) {
-    console.error('Error generating sitemap:', error);
-  }
-
-  // XML 푸터 작성
-  await writeChunk(`
-</urlset>`);
-
-  writer.close();
-
-  return new Response(stream.readable, {
-    headers: {
-      'Content-Type': 'text/xml',
-      'Cache-Control': 'public, max-age=28800, stale-while-revalidate=28800',
-    },
-  });
-}
+export default () => null;
