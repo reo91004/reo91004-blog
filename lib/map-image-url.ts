7070596d7a1723a623bafbf5060e11bfec95cb18
reo91004 <reo91004@gmail.com>
2024-10-20 02:14:44 +0900

    feat: 이미지 처리방식 변경

--- lib/map-image-url.ts
+++ lib/map-image-url.ts
@@ -1,16 +1,12 @@
 import { Block } from 'notion-types';
 import { defaultMapImageUrl } from 'react-notion-x';
-import { defaultPageIcon, defaultPageCover } from './config';
+
+import { defaultPageCover, defaultPageIcon } from './config';
 
 export const mapImageUrl = (url: string, block: Block) => {
-  if (!url) return '';
-
   if (url === defaultPageCover || url === defaultPageIcon) {
     return url;
   }
 
-  // Encode the URL for Notion's caching mechanism
-  const encodedUrl = encodeURIComponent(url);
-  const id = block?.id;
-  return `https://reo91004.notion.site/image/${encodedUrl}?table=block&id=${id}&cache=v2`;
+  return defaultMapImageUrl(url, block);
 };
